2d423bdffc1e4001ef8b9f3599414003a4f32a3e
bmykhaylivvv <bmykhaylivvv@gmail.com>
2021-10-31 22:34:40 +0200

    Add Delivery strategies, Payment strategies, Decorators and cover all this code with tests

--- src/test/java/payment/PaymentTest.java
+++ src/test/java/payment/PaymentTest.java
@@ -1,4 +1,13 @@
 package payment;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class PaymentTest {
+    private CreditCardPaymentStrategy creditCardPaymentStrategy;
+    private PayPalPaymentStrategy payPalPaymentStrategy;
+
+    @BeforeEach
+    void setUp() {
+
+    }
 }
